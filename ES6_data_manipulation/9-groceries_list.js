aa4b26efc61dbf8bcc290cd0ab326526d7ad3a4e
TommyBryan <tommy0713@gmail.com>
2025-04-06 22:56:56 -0400

    Added comments for clarification

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,15 @@
 export default function groceriesList() {
+  // Create a new Map object to store grocery items and their quantities
   const groceries = new Map();
 
+  // Add grocery items to the Map using the set() method
+  // Each item is a key (the name of the grocery) with a value (its quantity)
   groceries.set('Apples', 10);
   groceries.set('Tomatoes', 10);
   groceries.set('Pasta', 1);
   groceries.set('Rice', 1);
   groceries.set('Banana', 5);
 
+  // Return the filled Map object
   return groceries;
 }
