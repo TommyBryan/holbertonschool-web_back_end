302310047287ff89c75d66b1e1618bf260fe372b
TommyBryan <tommy0713@gmail.com>
2025-04-06 23:05:45 -0400

    Task 10

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+// Function to update the quantities of unique items in a Map
+export default function updateUniqueItems(groceries) {
+  // Check if the input is a Map; if not, throw an error
+  if (!(groceries instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  // Iterate through the Map entries
+  for (const [item, quantity] of groceries.entries()) {
+    // If the quantity is not 1 (not unique), update it to 100
+    if (quantity !== 1) {
+      groceries.set(item, 100);
+    }
+  }
+}
