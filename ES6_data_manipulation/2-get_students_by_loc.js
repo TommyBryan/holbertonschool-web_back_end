743701dd088c9fc4e78b39d026d7fae43e363303
TommyBryan <tommy0713@gmail.com>
2025-08-25 13:18:35 -0400

    New tasks

--- ES6_data_manipulation/2-get_students_by_loc.js
+++
@@ -1,7 +0,0 @@
-export default function getStudentsByLocation(students, city) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-
-  return students.filter((students) => students.location === city);
-}
--- ES6_data_manipulation/6-set.js
+++
@@ -1,6 +0,0 @@
-// Define and export a function named setFromArray
-export default function setFromArray(array) {
-  // Create a new Set from the input array
-  // A Set automatically removes duplicate values
-  return new Set(array);
-}
