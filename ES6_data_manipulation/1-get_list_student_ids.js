38494391ad3123342e34d607841ac691366faa39
TommyBryan <tommybryan0713@gmail.com>
2025-08-26 12:33:49 -0400

    Task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,8 @@
-export default function getListStudentsIds() {
-
+export default function getListStudentIds(students) {
+  // Check if students is an array if not return an empty array
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  // Map the students array to an array of student IDs
+  return students.map((student) => student.id);
 }
