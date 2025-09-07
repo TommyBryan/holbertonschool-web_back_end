98338d3de40742af708e93e5f46c13d24fcc1df6
TommyBryan <tommybryan0713@gmail.com>
2025-09-07 15:11:16 -0400

    Task 4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,8 +1,19 @@
-// Import http module
+// Import the http module
 const http = require('http');
 
 const port = 1245;
 
+// Create the server
 const app = http.createServer((req, res) => {
-    
-})
+  // Set response header to plain text
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // Send the response
+  res.end('Hello Holberton School!');
+});
+
+// Make the server listen on port 1245
+app.listen(port);
+
+// Export the app variable
+module.exports = app;
+
