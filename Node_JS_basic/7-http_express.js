853bae628fc6287cb536ec8bd6ddb61f79ebf4d5
TommyBryan <tommy0713@gmail.com>
2025-09-07 23:07:06 -0400

    Task 7

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,35 @@
+// Import express module
+const express = require('express');
+
+// Get command line arguments (database file path)
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+// Set database file path from arguments
+const DATABASE = args[0];
+
+// Create an Express application
+const app = express();
+
+// Define root route
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+// Define /students route
+app.get('/students', async (request, response) => {
+  const msg = 'This is the list of our students\n';
+  try {
+    // Call countStudents and await its result
+    const students = await countStudents(DATABASE);
+    response.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    // If error, send error message
+    response.send(`${msg}${error.message}`);
+  }
+});
+
+// Start the server on port 1245
+app.listen(1245);
+
+module.exports = app;
