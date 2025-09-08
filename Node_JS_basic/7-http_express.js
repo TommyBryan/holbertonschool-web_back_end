0dcdb89a82b486cb01fd9340b051f6f698c7cde9
TommyBryan <tommy0713@gmail.com>
2025-09-07 23:34:51 -0400

    Fix response formatting in / and /students routes

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -13,16 +13,18 @@ const app = express();
 
 // Define root route
 app.get('/', (request, response) => {
+  response.set('Content-Type', 'text/plain');
   response.send('Hello Holberton School!');
 });
 
 // Define /students route
 app.get('/students', async (request, response) => {
+  response.set('Content-Type', 'text/plain');
   const msg = 'This is the list of our students\n';
   try {
     // Call countStudents and await its result
     const students = await countStudents(DATABASE);
-    response.send(`${msg}${students.join('\n')}`);
+    response.send(`${msg}${students}`);
   } catch (error) {
     // If error, send error message
     response.send(`${msg}${error.message}`);
